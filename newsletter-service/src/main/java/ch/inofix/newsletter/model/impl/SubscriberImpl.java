0c8a90aa50a877c489291f7a56c5b217be26ccbb
Christian Berndt <berndt@inofix.ch>
2017-02-19 16:41:23 +0100

    Configured sample unit test.

--- newsletter-service/src/main/java/ch/inofix/newsletter/model/impl/SubscriberImpl.java
+++ newsletter-service/src/main/java/ch/inofix/newsletter/model/impl/SubscriberImpl.java
@@ -40,27 +40,4 @@ public class SubscriberImpl extends SubscriberBaseImpl {
 	 */
 	public SubscriberImpl() {
 	}
-
-	public SubscriberImpl(String firstName, String middleName, String lastName) {
-		_firstName = firstName;
-		_middleName = middleName;
-		_lastName = lastName;
-	}
-
-	public int fullNameLength() {
-		return _firstName.length() + _middleName.length() + _lastName.length();
-	}
-
-	public String getMiddleInitial() {
-		return _middleName.charAt(0) + ".";
-	}
-
-	@Override
-	public String toString() {
-		return _firstName + " " + getMiddleInitial() + " " + _lastName;
-	}
-
-	private String _firstName;
-	private String _middleName;
-	private String _lastName;
 }
