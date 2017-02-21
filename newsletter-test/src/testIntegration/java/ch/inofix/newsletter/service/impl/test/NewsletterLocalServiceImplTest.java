8fc7baaa8c78a9061da8b14d4473ed40f5aad6f0
Christian Berndt <berndt@inofix.ch>
2017-02-21 13:38:59 +0100

    Disabled import with unmet dependencies.

--- newsletter-test/src/testIntegration/java/ch/inofix/newsletter/service/impl/test/NewsletterLocalServiceImplTest.java
+++ newsletter-test/src/testIntegration/java/ch/inofix/newsletter/service/impl/test/NewsletterLocalServiceImplTest.java
@@ -1,17 +1,16 @@
 package ch.inofix.newsletter.service.impl.test;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
+import org.junit.Assert;
+//import org.junit.ClassRule;
+//import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.Sync;
-import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+//import com.liferay.portal.kernel.service.ServiceContext;
+//import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+//import com.liferay.portal.kernel.test.rule.Sync;
+//import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 
 /**
  * 
@@ -22,24 +21,21 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
  *
  */
 @RunWith(Arquillian.class)
-@Sync
+//@Sync
 public class NewsletterLocalServiceImplTest {
 	
-	@ClassRule
-	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			SynchronousDestinationTestRule.INSTANCE);
+//	@ClassRule
+//	@Rule
+//	public static final AggregateTestRule aggregateTestRule =
+//		new AggregateTestRule();
 	
 	@Test
 	public void testAddNewsletter() throws Exception {
 		
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext();
+//		ServiceContext serviceContext =
+//			ServiceContextTestUtil.getServiceContext();
 		
-		
-		
+		Assert.assertEquals(1, 1);
 		
 	}
 
