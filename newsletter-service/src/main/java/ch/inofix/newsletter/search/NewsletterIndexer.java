6d7f949b0372cbcd60aae2a20fb775dd4fb42357
Christian Berndt <berndt@inofix.ch>
2017-03-16 17:14:37 +0100

    Added missing component annotation.

--- newsletter-service/src/main/java/ch/inofix/newsletter/search/NewsletterIndexer.java
+++ newsletter-service/src/main/java/ch/inofix/newsletter/search/NewsletterIndexer.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 
+import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
@@ -19,6 +20,7 @@ import com.liferay.portal.kernel.search.BaseIndexer;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
+import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -32,131 +34,132 @@ import ch.inofix.newsletter.service.permission.NewsletterPermission;
 /**
  * @author Christian Berndt
  * @created 2016-10-15 23:14
- * @modified 2017-03-16 16:06
- * @version 1.0.1
+ * @modified 2017-03-16 16:49
+ * @version 1.0.2
  */
+@Component(immediate = true, service = Indexer.class)
 public class NewsletterIndexer extends BaseIndexer<Newsletter> {
 
-	public static final String CLASS_NAME = Newsletter.class.getName();
+    public static final String CLASS_NAME = Newsletter.class.getName();
 
-	public NewsletterIndexer() {
-		setDefaultSelectedFieldNames(Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
-				Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE, Field.UID,
-				Field.URL);
-		setFilterSearch(true);
-		setPermissionAware(true);
-	}
+    public NewsletterIndexer() {
+        setDefaultSelectedFieldNames(Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
+                Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE, Field.UID,
+                Field.URL);
+        setFilterSearch(true);
+        setPermissionAware(true);
+    }
 
-	@Override
-	public String getClassName() {
-		return CLASS_NAME;
-	}
+    @Override
+    public String getClassName() {
+        return CLASS_NAME;
+    }
 
-	@Override
-	public boolean hasPermission(PermissionChecker permissionChecker, String entryClassName, long entryClassPK,
-			String actionId) throws Exception {
-		return NewsletterPermission.contains(permissionChecker, entryClassPK, ActionKeys.VIEW);
-	}
+    @Override
+    public boolean hasPermission(PermissionChecker permissionChecker, String entryClassName, long entryClassPK,
+            String actionId) throws Exception {
+        return NewsletterPermission.contains(permissionChecker, entryClassPK, ActionKeys.VIEW);
+    }
 
-	@Override
-	protected void doDelete(Newsletter newsletter) throws Exception {
-		deleteDocument(newsletter.getCompanyId(), newsletter.getNewsletterId());
-	}
+    @Override
+    protected void doDelete(Newsletter newsletter) throws Exception {
+        deleteDocument(newsletter.getCompanyId(), newsletter.getNewsletterId());
+    }
 
-	@Override
-	protected Document doGetDocument(Newsletter newsletter) throws Exception {
-		Document document = getBaseModelDocument(CLASS_NAME, newsletter);
-		document.addTextSortable(Field.TITLE, newsletter.getTitle());
+    @Override
+    protected Document doGetDocument(Newsletter newsletter) throws Exception {
+        Document document = getBaseModelDocument(CLASS_NAME, newsletter);
+        document.addTextSortable(Field.TITLE, newsletter.getTitle());
 
-		return document;
-	}
+        return document;
+    }
 
-	@Override
-	protected Summary doGetSummary(Document document, Locale locale, String snippet, PortletRequest portletRequest,
-			PortletResponse portletResponse) throws Exception {
+    @Override
+    protected Summary doGetSummary(Document document, Locale locale, String snippet, PortletRequest portletRequest,
+            PortletResponse portletResponse) throws Exception {
 
-		Summary summary = createSummary(document, Field.TITLE, Field.URL);
+        Summary summary = createSummary(document, Field.TITLE, Field.URL);
 
-		return summary;
-	}
+        return summary;
+    }
 
-	@Override
-	protected void doReindex(String className, long classPK) throws Exception {
+    @Override
+    protected void doReindex(String className, long classPK) throws Exception {
 
-		Newsletter newsletter = _newsletterLocalService.getNewsletter(classPK);
+        Newsletter newsletter = _newsletterLocalService.getNewsletter(classPK);
 
-		doReindex(newsletter);
-	}
+        doReindex(newsletter);
+    }
 
-	@Override
-	protected void doReindex(String[] ids) throws Exception {
+    @Override
+    protected void doReindex(String[] ids) throws Exception {
 
-		long companyId = GetterUtil.getLong(ids[0]);
+        long companyId = GetterUtil.getLong(ids[0]);
 
-		// TODO: what about the group?
-		reindexNewsletters(companyId);
-		// reindexNewsletters(companyId, groupId);
-	}
+        // TODO: what about the group?
+        reindexNewsletters(companyId);
+        // reindexNewsletters(companyId, groupId);
+    }
 
-	@Override
-	protected void doReindex(Newsletter newsletter) throws Exception {
+    @Override
+    protected void doReindex(Newsletter newsletter) throws Exception {
 
-		Document document = getDocument(newsletter);
+        Document document = getDocument(newsletter);
 
-		IndexWriterHelperUtil.updateDocument(getSearchEngineId(), newsletter.getCompanyId(), document,
-				isCommitImmediately());
-	}
+        IndexWriterHelperUtil.updateDocument(getSearchEngineId(), newsletter.getCompanyId(), document,
+                isCommitImmediately());
+    }
 
-	protected void reindexNewsletters(long companyId) throws PortalException {
+    protected void reindexNewsletters(long companyId) throws PortalException {
 
-		final IndexableActionableDynamicQuery indexableActionableDynamicQuery = _newsletterLocalService
-				.getIndexableActionableDynamicQuery();
+        final IndexableActionableDynamicQuery indexableActionableDynamicQuery = _newsletterLocalService
+                .getIndexableActionableDynamicQuery();
 
-		indexableActionableDynamicQuery.setAddCriteriaMethod(new ActionableDynamicQuery.AddCriteriaMethod() {
+        indexableActionableDynamicQuery.setAddCriteriaMethod(new ActionableDynamicQuery.AddCriteriaMethod() {
 
-			@Override
-			public void addCriteria(DynamicQuery dynamicQuery) {
+            @Override
+            public void addCriteria(DynamicQuery dynamicQuery) {
 
-				Property statusProperty = PropertyFactoryUtil.forName("status");
+                Property statusProperty = PropertyFactoryUtil.forName("status");
 
-				Integer[] statuses = { WorkflowConstants.STATUS_APPROVED, WorkflowConstants.STATUS_IN_TRASH };
+                Integer[] statuses = { WorkflowConstants.STATUS_APPROVED, WorkflowConstants.STATUS_IN_TRASH };
 
-				dynamicQuery.add(statusProperty.in(statuses));
-			}
+                dynamicQuery.add(statusProperty.in(statuses));
+            }
 
-		});
-		indexableActionableDynamicQuery.setCompanyId(companyId);
-		// TODO: what about the group?
-		// indexableActionableDynamicQuery.setGroupId(groupId);
-		indexableActionableDynamicQuery
-				.setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Newsletter>() {
+        });
+        indexableActionableDynamicQuery.setCompanyId(companyId);
+        // TODO: what about the group?
+        // indexableActionableDynamicQuery.setGroupId(groupId);
+        indexableActionableDynamicQuery
+                .setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Newsletter>() {
 
-					@Override
-					public void performAction(Newsletter newsletter) {
-						try {
-							Document document = getDocument(newsletter);
+                    @Override
+                    public void performAction(Newsletter newsletter) {
+                        try {
+                            Document document = getDocument(newsletter);
 
-							indexableActionableDynamicQuery.addDocuments(document);
-						} catch (PortalException pe) {
-							if (_log.isWarnEnabled()) {
-								_log.warn("Unable to index newsletter " + newsletter.getNewsletterId(), pe);
-							}
-						}
-					}
+                            indexableActionableDynamicQuery.addDocuments(document);
+                        } catch (PortalException pe) {
+                            if (_log.isWarnEnabled()) {
+                                _log.warn("Unable to index newsletter " + newsletter.getNewsletterId(), pe);
+                            }
+                        }
+                    }
 
-				});
-		indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
+                });
+        indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
 
-		indexableActionableDynamicQuery.performActions();
-	}
+        indexableActionableDynamicQuery.performActions();
+    }
 
-	@Reference(unbind = "-")
-	protected void setNewsletterLocalService(NewsletterLocalService newsletterLocalService) {
+    @Reference(unbind = "-")
+    protected void setNewsletterLocalService(NewsletterLocalService newsletterLocalService) {
 
-		_newsletterLocalService = newsletterLocalService;
-	}
+        _newsletterLocalService = newsletterLocalService;
+    }
 
-	private static final Log _log = LogFactoryUtil.getLog(NewsletterIndexer.class);
+    private static final Log _log = LogFactoryUtil.getLog(NewsletterIndexer.class);
 
-	private NewsletterLocalService _newsletterLocalService;
+    private NewsletterLocalService _newsletterLocalService;
 }
--- newsletter-service/src/main/java/ch/inofix/newsletter/search/SubscriberIndexer.java
+++ newsletter-service/src/main/java/ch/inofix/newsletter/search/SubscriberIndexer.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 
+import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
@@ -19,6 +20,7 @@ import com.liferay.portal.kernel.search.BaseIndexer;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
+import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -32,131 +34,132 @@ import ch.inofix.newsletter.service.permission.SubscriberPermission;
 /**
  * @author Christian Berndt
  * @created 2017-03-16 16:19
- * @modified 2017-03-16 16:19
- * @version 1.0.0
+ * @modified 2017-03-16 16:50
+ * @version 1.0.1
  */
+@Component(immediate = true, service = Indexer.class)
 public class SubscriberIndexer extends BaseIndexer<Subscriber> {
 
-	public static final String CLASS_NAME = Subscriber.class.getName();
+    public static final String CLASS_NAME = Subscriber.class.getName();
 
-	public SubscriberIndexer() {
-		setDefaultSelectedFieldNames(Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
-				Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE, Field.UID,
-				Field.URL);
-		setFilterSearch(true);
-		setPermissionAware(true);
-	}
+    public SubscriberIndexer() {
+        setDefaultSelectedFieldNames(Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
+                Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE, Field.UID,
+                Field.URL);
+        setFilterSearch(true);
+        setPermissionAware(true);
+    }
 
-	@Override
-	public String getClassName() {
-		return CLASS_NAME;
-	}
+    @Override
+    public String getClassName() {
+        return CLASS_NAME;
+    }
 
-	@Override
-	public boolean hasPermission(PermissionChecker permissionChecker, String entryClassName, long entryClassPK,
-			String actionId) throws Exception {
-		return SubscriberPermission.contains(permissionChecker, entryClassPK, ActionKeys.VIEW);
-	}
+    @Override
+    public boolean hasPermission(PermissionChecker permissionChecker, String entryClassName, long entryClassPK,
+            String actionId) throws Exception {
+        return SubscriberPermission.contains(permissionChecker, entryClassPK, ActionKeys.VIEW);
+    }
 
-	@Override
-	protected void doDelete(Subscriber subscriber) throws Exception {
-		deleteDocument(subscriber.getCompanyId(), subscriber.getSubscriberId());
-	}
+    @Override
+    protected void doDelete(Subscriber subscriber) throws Exception {
+        deleteDocument(subscriber.getCompanyId(), subscriber.getSubscriberId());
+    }
 
-	@Override
-	protected Document doGetDocument(Subscriber subscriber) throws Exception {
-		Document document = getBaseModelDocument(CLASS_NAME, subscriber);
-		document.addTextSortable(Field.TITLE, subscriber.getTitle());
+    @Override
+    protected Document doGetDocument(Subscriber subscriber) throws Exception {
+        Document document = getBaseModelDocument(CLASS_NAME, subscriber);
+        document.addTextSortable(Field.TITLE, subscriber.getTitle());
 
-		return document;
-	}
+        return document;
+    }
 
-	@Override
-	protected Summary doGetSummary(Document document, Locale locale, String snippet, PortletRequest portletRequest,
-			PortletResponse portletResponse) throws Exception {
+    @Override
+    protected Summary doGetSummary(Document document, Locale locale, String snippet, PortletRequest portletRequest,
+            PortletResponse portletResponse) throws Exception {
 
-		Summary summary = createSummary(document, Field.TITLE, Field.URL);
+        Summary summary = createSummary(document, Field.TITLE, Field.URL);
 
-		return summary;
-	}
+        return summary;
+    }
 
-	@Override
-	protected void doReindex(String className, long classPK) throws Exception {
+    @Override
+    protected void doReindex(String className, long classPK) throws Exception {
 
-		Subscriber subscriber = _subscriberLocalService.getSubscriber(classPK);
+        Subscriber subscriber = _subscriberLocalService.getSubscriber(classPK);
 
-		doReindex(subscriber);
-	}
+        doReindex(subscriber);
+    }
 
-	@Override
-	protected void doReindex(String[] ids) throws Exception {
+    @Override
+    protected void doReindex(String[] ids) throws Exception {
 
-		long companyId = GetterUtil.getLong(ids[0]);
+        long companyId = GetterUtil.getLong(ids[0]);
 
-		// TODO: what about the group?
-		reindexSubscribers(companyId);
-		// reindexSubscribers(companyId, groupId);
-	}
+        // TODO: what about the group?
+        reindexSubscribers(companyId);
+        // reindexSubscribers(companyId, groupId);
+    }
 
-	@Override
-	protected void doReindex(Subscriber subscriber) throws Exception {
+    @Override
+    protected void doReindex(Subscriber subscriber) throws Exception {
 
-		Document document = getDocument(subscriber);
+        Document document = getDocument(subscriber);
 
-		IndexWriterHelperUtil.updateDocument(getSearchEngineId(), subscriber.getCompanyId(), document,
-				isCommitImmediately());
-	}
+        IndexWriterHelperUtil.updateDocument(getSearchEngineId(), subscriber.getCompanyId(), document,
+                isCommitImmediately());
+    }
 
-	protected void reindexSubscribers(long companyId) throws PortalException {
+    protected void reindexSubscribers(long companyId) throws PortalException {
 
-		final IndexableActionableDynamicQuery indexableActionableDynamicQuery = _subscriberLocalService
-				.getIndexableActionableDynamicQuery();
+        final IndexableActionableDynamicQuery indexableActionableDynamicQuery = _subscriberLocalService
+                .getIndexableActionableDynamicQuery();
 
-		indexableActionableDynamicQuery.setAddCriteriaMethod(new ActionableDynamicQuery.AddCriteriaMethod() {
+        indexableActionableDynamicQuery.setAddCriteriaMethod(new ActionableDynamicQuery.AddCriteriaMethod() {
 
-			@Override
-			public void addCriteria(DynamicQuery dynamicQuery) {
+            @Override
+            public void addCriteria(DynamicQuery dynamicQuery) {
 
-				Property statusProperty = PropertyFactoryUtil.forName("status");
+                Property statusProperty = PropertyFactoryUtil.forName("status");
 
-				Integer[] statuses = { WorkflowConstants.STATUS_APPROVED, WorkflowConstants.STATUS_IN_TRASH };
+                Integer[] statuses = { WorkflowConstants.STATUS_APPROVED, WorkflowConstants.STATUS_IN_TRASH };
 
-				dynamicQuery.add(statusProperty.in(statuses));
-			}
+                dynamicQuery.add(statusProperty.in(statuses));
+            }
 
-		});
-		indexableActionableDynamicQuery.setCompanyId(companyId);
-		// TODO: what about the group?
-		// indexableActionableDynamicQuery.setGroupId(groupId);
-		indexableActionableDynamicQuery
-				.setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Subscriber>() {
+        });
+        indexableActionableDynamicQuery.setCompanyId(companyId);
+        // TODO: what about the group?
+        // indexableActionableDynamicQuery.setGroupId(groupId);
+        indexableActionableDynamicQuery
+                .setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Subscriber>() {
 
-					@Override
-					public void performAction(Subscriber subscriber) {
-						try {
-							Document document = getDocument(subscriber);
+                    @Override
+                    public void performAction(Subscriber subscriber) {
+                        try {
+                            Document document = getDocument(subscriber);
 
-							indexableActionableDynamicQuery.addDocuments(document);
-						} catch (PortalException pe) {
-							if (_log.isWarnEnabled()) {
-								_log.warn("Unable to index subscriber " + subscriber.getSubscriberId(), pe);
-							}
-						}
-					}
+                            indexableActionableDynamicQuery.addDocuments(document);
+                        } catch (PortalException pe) {
+                            if (_log.isWarnEnabled()) {
+                                _log.warn("Unable to index subscriber " + subscriber.getSubscriberId(), pe);
+                            }
+                        }
+                    }
 
-				});
-		indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
+                });
+        indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
 
-		indexableActionableDynamicQuery.performActions();
-	}
+        indexableActionableDynamicQuery.performActions();
+    }
 
-	@Reference(unbind = "-")
-	protected void setSubscriberLocalService(SubscriberLocalService subscriberLocalService) {
+    @Reference(unbind = "-")
+    protected void setSubscriberLocalService(SubscriberLocalService subscriberLocalService) {
 
-		_subscriberLocalService = subscriberLocalService;
-	}
+        _subscriberLocalService = subscriberLocalService;
+    }
 
-	private static final Log _log = LogFactoryUtil.getLog(SubscriberIndexer.class);
+    private static final Log _log = LogFactoryUtil.getLog(SubscriberIndexer.class);
 
-	private SubscriberLocalService _subscriberLocalService;
+    private SubscriberLocalService _subscriberLocalService;
 }
