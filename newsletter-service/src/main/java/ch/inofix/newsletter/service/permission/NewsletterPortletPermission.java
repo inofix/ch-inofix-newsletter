b2b798c20cb86e093a16134abdbfbe841b03e534
Christian Berndt <berndt@inofix.ch>
2018-01-29 12:11:03 +0100

    Upgrade NewsletterPortletPermission to 7.0.x.

--- newsletter-service/src/main/java/ch/inofix/newsletter/service/permission/NewsletterPortletPermission.java
+++ newsletter-service/src/main/java/ch/inofix/newsletter/service/permission/NewsletterPortletPermission.java
@@ -2,17 +2,21 @@ package ch.inofix.newsletter.service.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.BaseResourcePermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import ch.inofix.newsletter.constants.PortletKeys;
+
 
 /**
  *
  * @author Christian Berndt
  * @created 2016-10-08 00:34
- * @modified 2017-03-10 16:36
- * @version 1.0.1
+ * @modified 2018-01-29 11:29
+ * @version 1.0.2
  *
  */
-public class NewsletterPortletPermission {
+public class NewsletterPortletPermission extends BaseResourcePermissionChecker {
 
     public static final String RESOURCE_NAME = "ch.inofix.newsletter";
 
@@ -20,12 +24,18 @@ public class NewsletterPortletPermission {
             throws PortalException {
 
         if (!contains(permissionChecker, groupId, actionId)) {
+
             throw new PrincipalException();
         }
     }
 
     public static boolean contains(PermissionChecker permissionChecker, long groupId, String actionId) {
 
-        return permissionChecker.hasPermission(groupId, RESOURCE_NAME, groupId, actionId);
+        return contains(permissionChecker, RESOURCE_NAME, PortletKeys.NEWSLETTER_MANAGER, groupId, actionId);
+    }
+    
+    @Override
+    public Boolean checkResource(PermissionChecker permissionChecker, long classPK, String actionId) {
+        return contains(permissionChecker, classPK, actionId);
     }
 }
